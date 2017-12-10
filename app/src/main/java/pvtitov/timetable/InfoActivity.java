8c1a89604e4310d56b479c3510a81b0cba792c10
Pavel Titov <pavel_titov@mail.ru>
2017-12-10 14:31:39 +0300

    Инфо. Копирайт, версия

--- app/src/main/java/pvtitov/timetable/InfoActivity.java
+++ app/src/main/java/pvtitov/timetable/InfoActivity.java
@@ -2,6 +2,7 @@ package pvtitov.timetable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        String appInfo = getResources().getString(R.string.app_info, BuildConfig.VERSION_NAME);
+
+        TextView textView = findViewById(R.id.textview_info);
+        textView.setText(appInfo);
     }
 }
